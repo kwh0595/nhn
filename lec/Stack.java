a2c807f37ee239e345df1b58dd8a68a8e07cf146
김우희 <kimwoohee@gim-uhuiui-noteubug.local>
2024-08-29 09:21:44 +0900

    Stack modify

--- lec/Stack.java
+++ lec/Stack.java
@@ -1,30 +1,50 @@
 public class Stack{
-    public static int stack[]= new int[100];
-    public static int size = 0;
+    public static int index = 0;
+    public static int []array = new int[100];
+
     public static boolean isEmpty(){
-        if(size==0){
+      if(index<=0){
+        return true;
+      }
+      return false;
+    }
+    public static boolean isFull(){
+        if(index>array.length){
             return true;
-        }return false;
+        }
+        return false;
     }
     public static void push(int n){
-        stack[size++]=n;
+        if(isFull()){
+            throw new ArrayIndexOutOfBoundsException();
+        }
+        array[index++] = n;
     }
-    public static int pop(){
-        return stack[--size];
+    public static int  pop(){
+       if(isEmpty()){
+            throw new ArrayIndexOutOfBoundsException();
+       }
+       return array[--index];
+
     }
     public static int top(){
-        int tmp = stack[size-1];
+        if(isEmpty()){
+            throw new ArrayIndexOutOfBoundsException();
+        }
+        int tmp = array[index]-1;
         return tmp;
+
     }
     public static void main(String []args){
         push(1);
         push(2);
-        System.out.println(top());
-        System.out.println(pop());
-        System.out.println(top());
+        push(3);
+        System.out.println(isFull());
         System.out.println(isEmpty());
         System.out.println(pop());
+        System.out.println(top());
+        System.out.println(pop());
+        System.out.println(pop());
         System.out.println(isEmpty());
-
     }
 }
